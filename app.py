4edc4afd54619e79cd19f257117eb37907310905
Christian Cerullo <christiancerullo@Christians-MBP.home>
2025-03-09 15:14:20 +0100

    Fix sintassi Python rimuovendo commento errato

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ def search_vinted(query, page=1):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/google-chrome"  # 
-Aggiunto percorso Chrome
+    chrome_options.binary_location = "/usr/bin/google-chrome"
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
